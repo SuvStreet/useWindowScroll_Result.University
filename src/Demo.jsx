24eab4c61afaf7c0005cf4a69273f4c576ed9209
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-16 15:41:46 +0300

    feat(useWindowScroll): Реализовать задачу.

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -11,6 +11,19 @@ export function Demo() {
         Scroll position x: {scroll.x}, y: {scroll.y}
       </p>
       <button onClick={() => scrollTo({ y: 0 })}>Scroll to top</button>
+      <button onClick={() => scrollTo({ x: 0 })}>Scroll to left</button>
+      <button
+        onClick={() =>
+          scrollTo({
+            x: document.documentElement.scrollWidth / 2 - window.innerWidth / 2,
+            y:
+              document.documentElement.scrollHeight / 2 -
+              window.innerHeight / 2,
+          })
+        }
+      >
+        Scroll to center
+      </button>
     </div>
   )
 }
