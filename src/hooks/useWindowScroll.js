5e45c2e9adfba322d236f1e8c4784ca2d21afd4c
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-16 15:41:46 +0300

    feat(useWindowScroll): Реализовать задачу.

--- src/hooks/useWindowScroll.js
+++ src/hooks/useWindowScroll.js
@@ -1,5 +1,20 @@
+import { useState } from 'react'
 import { useWindowEvent } from './useWindowEvent'
 
 export function useWindowScroll() {
+  const [scroll, setScroll] = useState({ x: window.scrollX, y: window.scrollY })
+
+  const scrollTo = ({ x = window.scrollX, y = window.scrollY }) => {
+    window.scrollBy({
+      top: y - window.scrollY,
+      left: x - window.scrollX,
+      behavior: 'smooth',
+    })
+  }
+
+  useWindowEvent('scroll', () => {
+    setScroll({ x: window.scrollX, y: window.scrollY })
+  })
+
   return [scroll, scrollTo]
 }
