a354823d7da5c78aa5c6bdfd284405157d7ead71
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-23 21:09:44 +0300

    feat(useWindowScroll): Пременить рекомандации.

--- src/hooks/useWindowScroll.js
+++ src/hooks/useWindowScroll.js
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { useWindowEvent } from './useWindowEvent'
 
 export function useWindowScroll() {
   const [scroll, setScroll] = useState({ x: window.scrollX, y: window.scrollY })
 
-  const scrollTo = ({ x = window.scrollX, y = window.scrollY }) => {
+  const scrollTo = useCallback(({ x = window.scrollX, y = window.scrollY }) => {
+    if (typeof window === 'undefined') return
+
     window.scrollBy({
       top: y - window.scrollY,
       left: x - window.scrollX,
       behavior: 'smooth',
     })
-  }
+  }, [])
 
   useWindowEvent('scroll', () => {
     setScroll({ x: window.scrollX, y: window.scrollY })
